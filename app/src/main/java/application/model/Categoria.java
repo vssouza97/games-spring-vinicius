e5c7cb0f0cbc647a89f6bb9f8e92c07e98de56be
Vinícius Souza <vssouza2102@gmail.com>
2024-10-11 01:40:25 +0000

    projeto novo

---
+++ app/src/main/java/application/model/Categoria.java
@@ -0,0 +1,20 @@
+package application.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    
+}
